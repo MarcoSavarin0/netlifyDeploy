8a29c1fdd0b0983c8d33baef96e981236201def5
MarcoSavarin0 <ateneo2003@gmail.com>
2024-05-24 16:08:23 -0300

    aplicando algunos fix

--- _astro/hoisted.2RDltpyl.js
+++
@@ -1,1 +0,0 @@
-import{t as a,l as e,s,a as t}from"./constants.HYn_zami.js";import"./Now.astro_astro_type_script_index_0_lang.igjttvJc.js";const i=document.querySelectorAll(".card"),n=[e,[i,{y:["40%","0%"],opacity:[0,1]},{at:"-0.1",duration:.4,delay:s(.5),easing:t({velocity:100,stiffness:50,damping:10})}]];a(n);
